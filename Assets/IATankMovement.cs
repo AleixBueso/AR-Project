8a0b1fa4388978f0e5e7224b0575a17b6de819da
Aleix Bueso <aleix.bueso@gmail.com>
2017-05-14 20:35:32 +0200

    IA

---
+++ Assets/IATankMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IATankMovement : MonoBehaviour {
+
+	public Transform target;
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		target = GameObject.FindGameObjectWithTag("Tower").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Vector3.Distance(gameObject.transform.position, target.position) > 2)
+			agent.SetDestination(target.position);
+	}
+}
---
+++ Assets/TankSpawnerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSpawnerScript : MonoBehaviour {
+
+	public float SpawnerTimer = 0;
+	public float TankSpawnTimer = 0;
+	public GameObject Tank;
+	public int Wave = 0;
+	public int BreakTime = 30;
+	public List<GameObject> Tanks;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(SpawnerTimer >= TankSpawnTimer)
+		{
+			GameObject CreatedTank = Instantiate(Tank, new Vector3(Random.Range(-6.28f, 7.6f), 0, Random.Range(-7.44f, 8.14f)), Quaternion.identity, GameObject.Find("Game").transform);
+			Tanks.Add(CreatedTank);
+
+			SpawnerTimer = 0;
+			TankSpawnTimer = Random.Range(4, 6);
+		}
+		SpawnerTimer += Time.deltaTime;
+	}
+}
